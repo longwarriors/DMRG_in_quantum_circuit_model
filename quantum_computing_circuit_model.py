27aa91cd65be97464b96790057998f5cbbb66b31
longwarriors <longwarriors@gmail.com>
2023-04-19 18:24:11 +0800

    test

--- quantum_computing_circuit_model.py
+++ quantum_computing_circuit_model.py
@@ -4,6 +4,8 @@ from torch import tensor
 from numpy import array
 
 class Hamiltonian:
+    """对称矩阵和厄密矩阵的本征值都是实数"""
+    
     Fermion_Sz = tensor([
         [1., 0.],
         [0., -1]
@@ -16,4 +18,11 @@ class Hamiltonian:
     ],dtype=torch.cfloat)
 
 if __name__ == "__main__":
-    print(torch.linalg.eigh(Hamiltonian.Boson_Sz))
+    val, vec = torch.linalg.eigh(Hamiltonian.Fermion_Sz)
+    spin_up_eigenvalue = val[1] # 1/2
+    spin_down_eigenvalue = val[0] # -1/2
+    spin_up_eigenvector = vec[:,1]
+    spin_down_eigenvector = vec[:,0]
+    
+    print(val)
+    print(vec)
